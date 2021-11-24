2af9e70433a989a193dc63b7ffb106c4e0dccfe9
Landrell Williams <dontess5@yahoo.com>
2021-11-23 22:46:27 -0500

    adjustments

--- server/server.js
+++ server/server.js
@@ -1,32 +1,25 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/book-search',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
+
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
